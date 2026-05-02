cce23c2fe58057fbc6b0bf97a34b33967fa50fc9
Humberto <titi310501@gmail.com>
2026-05-01 19:59:41 -0700

    Lección 1: Métodos HTTP y Query/Path Params

--- app/main.py
+++ app/main.py
@@ -1,10 +1,80 @@
-from fastapi import FastAPI
-from libros.libros import LIBROS
+from fastapi import FastAPI, Body
+from libros.libros import catalogo_libros
 
 app = FastAPI()
+
+catalogo = catalogo_libros()
 
 # Endpoint/Metodo GET, equivalente a READ en un CRUD. Usado para consultar informacion.
 # Endpoint para solo consultar libros.
 @app.get("/consultar-catalogo")
-async def get_catalogo():
-    return LIBROS
+async def consultar_catalogo():
+    return catalogo.get_catalogo()
+
+# Endpoint GET que busca un libro por id y retorna su informacion
+@app.get("/consultar-catalogo/{id}")
+async def buscar_libro_por_id(id: int):
+    if id > catalogo.get_tamano_catalogo() - 1:
+        return {
+            "mensaje": "no encontrado"
+        }
+    return catalogo.get_libro_por_id(id)
+
+
+@app.get("/consultar-catalogo")
+async def filtrar_libros_por_categoria(categoria: str):
+    lista_libros = []
+    for libro in catalogo.get_catalogo():
+        if libro.get('categoria').casefold() == categoria.casefold():
+            lista_libros.append(libro)
+    if not lista_libros:
+        return {
+            "mensaje": "no se encontraron libros"
+        }
+    return lista_libros
+
+
+@app.get("/consultar-catalogo/{autor}")
+async def filtrar_libros_por_autor(autor: str):
+    lista_libros = []
+    for libro in catalogo.get_catalogo():
+        if libro.get('autor').casefold() == autor.casefold():
+            lista_libros.append(libro)
+    if not lista_libros:
+        return {
+            "mensaje": "no se encontraron libros"
+        }
+    
+    return lista_libros
+
+
+@app.get("/consultar-catalogo/{autor}")
+async def filtrar_libros_por_autor_y_categoria(autor: str, categoria: str):
+    lista_libros = []
+    for libro in catalogo.get_catalogo():
+        if libro.get('categoria').casefold() == categoria.casefold() and libro.get('autor').casefold() == autor.casefold():
+            lista_libros.append(libro)
+    if not lista_libros:
+        return {
+            "mensaje": "no se encontraron libros"
+        }
+    return lista_libros
+
+
+@app.post("/agregar-libro")
+async def agregar_libro(libro = Body()):
+    catalogo.agregar_libro(libro)
+
+
+@app.delete("/eliminar-libro/{libro}")
+async def eliminar_libro_por_titulo(titulo_libro: str):
+    for libro in catalogo.get_catalogo():
+        if libro.get('titulo').casefold() == titulo_libro.casefold():
+            libro.pop()
+            return {
+                "mensaje": "libro borrado"
+            }
+    
+    return {
+        "mensaje": "libro no encontrado"
+    }
--- libros/libros.py
+++ libros/libros.py
@@ -1,10 +1,27 @@
-LIBROS = [
-    {'titulo': '1984', 'autor': 'George Orwell', 'categoria': 'Distopia'},
-    {'titulo': 'Un mundo feliz', 'autor': 'Aldous Huxley', 'categoria': 'Distopia'},
-    {'titulo': 'Rebelion en la granja', 'autor': 'George Orwell', 'categoria': 'Distopia'},
-    {'titulo': 'Buscando a Alaska', 'autor': 'John Green', 'categoria': 'Romance'},
-    {'titulo': 'Manifiesto comunista', 'autor': 'Karl Marx', 'categoria': 'Politica'},
-    {'titulo': 'Los carteles no existen', 'autor': 'Oswaldo Zavala', 'categoria': 'Politica'},
-    {'titulo': 'Cronica de una muerte anunciada', 'autor': 'Gabriel García Márquez', 'categoria': 'Suspenso'},
-    {'titulo': 'Algebra de Baldor', 'autor': 'Aurelio Baldor', 'categoria': 'Matematicas'},
-]
+class catalogo_libros:
+
+    LIBROS = [
+        {'titulo': '1984', 'autor': 'george orwell', 'categoria': 'distopia'},
+        {'titulo': 'Un mundo feliz', 'autor': 'aldous huxley', 'categoria': 'distopia'},
+        {'titulo': 'Rebelion en la granja', 'autor': 'george orwell', 'categoria': 'distopia'},
+        {'titulo': 'Buscando a Alaska', 'autor': 'john green', 'categoria': 'misterio'},
+        {'titulo': 'Manifiesto comunista', 'autor': 'karl marx', 'categoria': 'politica'},
+        {'titulo': 'Los carteles no existen', 'autor': 'oswaldo zavala', 'categoria': 'politica'},
+        {'titulo': 'Cronica de una muerte anunciada', 'autor': 'gabriel garcía márquez', 'categoria': 'suspenso'},
+        {'titulo': 'Algebra de Baldor', 'autor': 'aurelio baldor', 'categoria': 'matematicas'},
+    ]
+
+    def __init__(self):
+        pass
+
+    def get_catalogo(self):
+        return self.LIBROS
+    
+    def get_libro_por_id(self, id: int):
+        return self.LIBROS[id]
+    
+    def get_tamano_catalogo(self):
+        return len(self.LIBROS)
+    
+    def agregar_libro(self, libro):
+        self.LIBROS.append(libro)
